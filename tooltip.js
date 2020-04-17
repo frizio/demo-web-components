4b12477786ad2da0edc3f77298ce325a078bb334
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-04-17 15:02:54 +0200

    Web Component Lifecycle

--- tooltip.js
+++ tooltip.js
@@ -5,7 +5,25 @@ class Tooltip extends HTMLElement {
   
   constructor() {
     super();
-    console.log('It is working!');
+    console.log('Element created: Basic Initialization.');
+    /*
+    const tooltipIcon = document.createElement('span');
+    tooltipIcon.textContent = ' (?)';
+    this.appendChild();
+    this.append(tooltipIcon);
+    */
+  }
+
+  connectedCallback() {
+    console.log('Element attached to DOM: DOM Initializations.');
+  }
+
+  disconnectedCallback() {
+    console.log('Element detaached from DOM: Cleanup Work.');
+  }
+
+  attributeChangedCallback() {
+    console.log('Observed attribute updated: Update Data + DOM.');
   }
 
 }
