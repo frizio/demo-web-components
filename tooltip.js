647404d33de744e742eb6281123c18f1b1e382e7
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-04-17 14:48:43 +0200

    First dummy custom element

--- tooltip.js
+++ tooltip.js
@@ -1,1 +1,17 @@
-console.log('Script tooltip here!!!')
+/* 
+ * Put here all the logic that make up the costum element
+ */
+class Tooltip extends HTMLElement {
+  
+  constructor() {
+    super();
+    console.log('It is working!');
+  }
+
+}
+
+customElements.define(
+  'lrm-tooltip',
+  Tooltip
+);
+
