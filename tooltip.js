27c3866e9b2d806e0b7d3c88bfdb0b8620025253
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-04-20 17:40:36 +0200

    Listening to Events Inside the component

--- tooltip.js
+++ tooltip.js
@@ -6,14 +6,28 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     console.log('Element created: Basic Initialization.');
+    this._tooltipContainer;
   }
 
   connectedCallback() {
     console.log('Element attached to DOM: DOM Initializations.');
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
   }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = 'This is the tooltip text!!!';
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
+  }
+
 
 }
 
