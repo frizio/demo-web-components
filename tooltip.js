ccac247fb4f7c24f564768f0895f3e4f47413f9f
Maurizio La Rocca <mrz.larocca@gmail.com>
2020-04-21 23:22:06 +0200

    Using the Shadow DOM

--- tooltip.js
+++ tooltip.js
@@ -8,6 +8,9 @@ class Tooltip extends HTMLElement {
     console.log('Element created: Basic Initialization.');
     this._tooltipContainer;
     this._tooltipText = 'Dummy tooltip default text';
+    this.attachShadow(
+      { mode: 'open' }
+    );
   }
   
   connectedCallback() {
@@ -19,7 +22,7 @@ class Tooltip extends HTMLElement {
     tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
     this.style.position = 'relative'
   }
   
@@ -32,7 +35,7 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer.style.position = 'absolute'
     this._tooltipContainer.style.zIndex = 10;
     
-    this.appendChild(this._tooltipContainer);
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
